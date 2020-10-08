d0622bf516ef634c233b53242a8b27cb7e2a6800
Dendi Handian <handian.dendi@gmail.com>
2020-10-08 18:16:50 +0700

    add materialui, add basic drawer

--- hoc/Layout/Layout.js
+++ hoc/Layout/Layout.js
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
 import Head from "next/head";
 import Aux from "../AuxHoc/AuxHoc";
+import { AppHeader } from "../../components/AppHeader/AppHeader";
+import { AppDrawer } from "../../components/AppDrawer/AppDrawer";
 
 export default class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
   render() {
     return (
       <Aux>
         <Head>
           <title>Elastic Exemplar</title>
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
         </Head>
+        <AppHeader drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <AppDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
         <main>{this.props.children}</main>
       </Aux>
     );
